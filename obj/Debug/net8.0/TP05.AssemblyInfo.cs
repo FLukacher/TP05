777374e1a7319914404badd0b8bbd17fd03cf345
DANTE MARTINEZ YUCOVSKY <49122259@ort>
2025-06-19 14:33:57 -0300

    diseño

--- obj/Debug/net8.0/TP05.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+434fedad079c9301c88252dc04bf578da7e2ea9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aaf3c1515b8dc223466ae2607a35a4f7efa0b5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
