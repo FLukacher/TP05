44ed45d1a5d86a544ca957cc00b2a92a7706095e
FACUNDO LUKACHER <49091985@ort>
2025-06-26 14:38:23 -0300

    .

--- obj/Debug/net8.0/TP05.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20360732ac249c4909d77a553a3239f57b9ed27a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd1a8d74ec7a596a0e2a23c0473722cbc6267bf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
